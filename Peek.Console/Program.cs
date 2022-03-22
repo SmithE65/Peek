b3b25bc9f66e8117f02e3ccdbc572142fe43015c
Eric Smith <ydrive65@yahoo.com>
2022-03-22 12:49:24 -0400

    working on learning method table

--- Peek.Console/Program.cs
+++ Peek.Console/Program.cs
@@ -7,57 +7,37 @@ Console.WriteLine(hello);
 var t1 = new Test() { MyString = hello, Prop1 = 0xABA, Prop2 = 0xABCD };
 var t2 = new Test() { MyString = hello, Prop1 = 0xDEF, Prop2 = 0x2468 };
 
-//Console.WriteLine(ToHex(Inspector.GetAddress(t1)));
-//Console.WriteLine(ToHex(Inspector.GetAddress(t2)));
+Console.WriteLine("A string...");
+Inspect("This is a string", "myString");
 
 lock (t2)
 {
     Inspect(t1, nameof(t1));
-    Inspect(t2, nameof(t2)); 
-}
-
-Console.WriteLine();
-Console.WriteLine("Method addresses...");
-unsafe
-{
-    delegate*<object, int> test = &GC.GetGeneration;
-    int gen = test(t1);
-    Console.WriteLine(ToHex((long)test));
-
-    delegate*<int, int> doSomethingPtr = &Inspector.DoSomething;
-    int i = doSomethingPtr(14);
-    Console.WriteLine($"{nameof(doSomethingPtr)}(14) = {i}");
-    Console.WriteLine(ToHex((long)doSomethingPtr));
-
-    byte* jmp = (byte*)doSomethingPtr;
-    if (*jmp == 0xE9) // Is 32-bit near jump?
-    {
-        int offset = *(int*)(jmp + 1);
-        var method = jmp + offset + 5;
-    }
-
-    Console.WriteLine("Break here.");
+    Inspect(t2, nameof(t2));
 }
 
 Console.WriteLine("Done");
 
-string ToHex(long l) => $"{l:X16}";
-
 static void Inspect(object o, string varName)
 {
+    var type = o.GetType();
     Console.WriteLine();
-    Console.WriteLine($"Inspecting {varName}...");
+    Console.WriteLine($"Inspecting {type.Name} ({varName})...");
+
+    var methods = type.GetMethods();
+    var properties = type.GetProperties();
 
     // Get instance data
     var f = Inspector.GetFields(o);
+    var root = f.MinBy(x => x.Address).Address;
 
     // Get type info
-    var header = Inspector.Peek(f.MinBy(x => x.Address).Address - 16, 8);
-    Console.WriteLine($"{string.Concat(header.Select(x => $"{x:X2}"))} - Header");
+    var header = Inspector.Peek(root - 16, 8);
+    Console.WriteLine($"{(root - 16):X16} - {string.Join(' ', header.Select(x => $"{x:X2}"))} - Header");
 
     // Get type info
-    var typeRef = Inspector.Peek(f.MinBy(x => x.Address).Address - 8, 8);
-    Console.WriteLine($"{string.Concat(typeRef.Select(x => $"{x:X2}"))} - Method Table Pointer");
+    var typeRef = Inspector.Peek(root - 8, 8);
+    Console.WriteLine($"{(root - 8):X16} - {string.Join(' ', typeRef.Select(x => $"{x:X2}"))} - Method Table Pointer");
 
     foreach (var (Name, Address, Size, Value) in f.OrderBy(x => x.Address))
     {
@@ -66,3 +46,28 @@ static void Inspect(object o, string varName)
         Console.WriteLine($"{Address:X16} - {string.Join(' ', bytes.Select(x => $"{x:X2}"))} - {Name} - {Value}");
     }
 }
+
+Console.ReadLine();
+
+//Console.WriteLine();
+//Console.WriteLine("Method addresses...");
+//unsafe
+//{
+//    delegate*<object, int> test = &GC.GetGeneration;
+//    int gen = test(t1);
+//    Console.WriteLine(ToHex((long)test));
+
+//    delegate*<int, int> doSomethingPtr = &Inspector.DoSomething;
+//    int i = doSomethingPtr(14);
+//    Console.WriteLine($"{nameof(doSomethingPtr)}(14) = {i}");
+//    Console.WriteLine(ToHex((long)doSomethingPtr));
+
+//    byte* jmp = (byte*)doSomethingPtr;
+//    if (*jmp == 0xE9) // Is 32-bit near jump?
+//    {
+//        int offset = *(int*)(jmp + 1);
+//        var method = jmp + offset + 5;
+//    }
+
+//    Console.WriteLine("Break here.");
+//}
